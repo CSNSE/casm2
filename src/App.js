b42779faac2c85ca6e99d7629350919129c757ee
spatel2024 <soniapatel270@gmail.com>
2024-03-11 11:30:00 -0400

    #38

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import {Nav, Links, Skin, Heart, Eye, Hand, Kidney, Leg, Liver, ConsentForm,Contact,About, Hprofile, Profile, Survey,Ty,Pic, Title,SurveyModelEyeCollection, ModelVidCollection, TodoCreateForm} from "./ui-components";
+import {Nav, Links, Skin, Heart, Eye, Hand, Kidney, Leg, Liver, ConsentForm,Contact,About, Hprofile, Hcontact, Profile, Survey,Ty,Pic, Title,SurveyModelEyeCollection, ModelVidCollection, TodoCreateForm, MContactCollection} from "./ui-components";
 import {Routes, Route} from 'react-router-dom'
 import EditF from './EditF'
 class App extends Component {
   render() {
   return (
     <div className="App"><header className="App-header">
-<Routes>
+<Routes>   
 <Route exact path='/' element={<div><Nav/><Title/></div>} />
 <Route exact path='/consent' element= {<div><Nav/><ConsentForm/></div>} />
 <Route exact path='/survey' element= {<div><Nav/><Survey/></div>} />
@@ -25,6 +25,7 @@ class App extends Component {
 <Route exact path='/model' element= {<div><Nav/><SurveyModelEyeCollection isPaginated itemsPerPage={1}/><ModelVidCollection isPaginated itemsPerPage={1}/><Ty/></div>} />
 <Route exact path='/edit/:cid' element= {<EditF/>} />
 <Route exact path='/contact' element= {<div><Nav/><Contact/></div>} />
+<Route exact path='/contactm' element= {<div><Nav/><Hcontact/><MContactCollection/></div>} />
 <Route exact path='/video' element= {<div><Nav/><TodoCreateForm/></div>} />
 <Route exact path='/profile' element= {<div><Nav/><Profile/></div>} />
 <Route exact path='/modv' element= {<div><Nav/><ModelVidCollection/></div>} />
